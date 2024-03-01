bc6e4426a91cd3a76f5f203445576c3dab9d4796
Tharmeshwaran Ganesh <tganesh@sisn.com>
2024-03-01 14:48:04 +0530

    Blazor project and API project consuming Api post endpoint in blazor and sending file through the Api endpoint

--- BlazorApp4/Program.cs
+++ BlazorApp4/Program.cs
@@ -1,5 +1,4 @@
-
-using BlazorApp1.Components;
+using BlazorApp4.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,17 +8,16 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
